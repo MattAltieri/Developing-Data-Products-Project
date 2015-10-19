ce91796f98bb77a187740981330944433a62de1b
MattAltieri <matt.onlystory@gmail.com>
2015-10-19 19:08:08 -0400

    Tweaked input controls. Need to add plot output section with selectable plot types.

--- Phone Agent Productivity Coach/server.R
+++ Phone Agent Productivity Coach/server.R
@@ -2,6 +2,8 @@ require(shiny)
 
 associates <<- as.character(read.csv('./Data/Associate Names.csv')$Names)
 prod.data <<- read.csv('./Data/Productivity Data.csv')
+prod.data$Total.Call.Time = prod.data$Talk.Time + prod.data$Hold.Time +
+    prod.data$Followup.Time
 
 shinyServer(function(input, output, session) {
     
--- Phone Agent Productivity Coach/ui.R
+++ Phone Agent Productivity Coach/ui.R
@@ -2,7 +2,15 @@ require(shiny)
 
 shinyUI(fluidPage(
     titlePanel("Phone Agent Productivity Coach"),
-    verticalLayout(
-        selectInput("assoc", "Associate Name", choices=NULL)
-    )
+    fluidRow(
+        column(2,
+            selectInput("assoc", h4("Associate Name"), choices=NULL)),
+        column(4,
+            dateRangeInput("dates", h4("Select Date Range"))),
+        column(2,
+               br(),
+               br(),
+               submitButton("Submit"))
+    ),
+    hr()
 ))
