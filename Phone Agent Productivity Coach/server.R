23a16548905061e6f54a1435b9e1b2ce575824aa
MattAltieri <matt.onlystory@gmail.com>
2015-10-22 23:39:59 -0400

    Enhanced reactivity, allowed drop-downs to govern which fields and functions are applied via  and . Prepped for plots next.

--- Phone Agent Productivity Coach/server.R
+++ Phone Agent Productivity Coach/server.R
@@ -2,50 +2,94 @@ require(shiny)
 require(ggplot2)
 require(dplyr)
 require(lubridate)
+require(lazyeval)
 
 associates <<- as.character(read.csv('./Data/Associate Names.csv')$Names)
 prod.data <<- read.csv('./Data/Productivity Data.csv')
 prod.data$Total.Call.Time <<- prod.data$Talk.Time + prod.data$Hold.Time +
     prod.data$Followup.Time
+prod.data <<- mutate(prod.data, Date=mdy(Date))
+
+lvl.dtl <<- as.data.frame(rbind(
+        c("Daily","as.character"), #FIX THIS
+        c("Weekly","week"),
+        c("Monthly","month"),
+        c("Quarterly","quarter")),
+    stringsAsFactors=F)
+names(lvl.dtl) <<- c("name","func")
+
+reports <<- as.data.frame(rbind(
+        c("Avg. Talk Time","Talk.Time"),
+        c("Avg. Hold Time","Hold.Time"),
+        c("Avg. Followup Time","Followup.Time"),
+        c("Avg. Total Call Time","Total.Call.Time")),
+    stringsAsFactors=F)
+names(reports) <<- c("name","var")
 
 shinyServer(function(input, output, session) {
     
     observe({
         updateSelectInput(session, "assoc",
             choices=c("Choose one"="",associates))
+        updateSelectInput(session, "lvl_dtl",
+            choices=c("Choose one"="",lvl.dtl$name))
+        updateSelectInput(session, "rpts",
+            choices=c("Choose one"="",reports$name))
     })
 
     output$assoc <- renderText({
-        paste(strong("Productivity for:"),input$assoc)
+        input$submit
+        isolate(paste(strong("Productivity for:"), input$assoc))
     })
     output$dates <- renderText({
-        paste(strong("Report Date:"),
-              input$dates[1],
-              "to",
-              input$dates[2])
+        input$submit
+        isolate(paste(strong("Report Date:"), input$dates[1],
+              "to", input$dates[2]))
+    })
+    output$lvl_dtl <- renderText({
+        input$submit
+        isolate(paste(strong("Report Granularity:"), input$lvl_dtl))
+    })
+    output$rpts <- renderText({
+        input$submit
+        isolate(paste(strong("Report Type:"), input$rpts))
     })
     
-    output$prod.plot <- renderPlot({
-        # Need selector for stat to examine
-        # Pull field names, add name of stat being calculated
-        # Use selector to add field for stat being calculated
-        # Apply names vector back to dataframe names
-        # Summarize data
-        # Plot data
-        assoc.data <- prod.data[which(prod.data==input$assoc),] %>%
-            group_by(week(Date)) %>%
-            summarize(sum(Calls),
-                      weighted.mean(Talk.Time, Calls),
-                      weighted.mean(Hold.Time, Calls),
-                      weighted.mean(Followup.Time, Calls),
-                      weighted.mean(Total.Call.Time, Calls))
-        sum.data <- prod.data %>%
-            group_by(week(Date)) %>%
-            summarize(sum(Calls),
-                      weighted.mean(Talk.Time, Calls),
-                      weighted.mean(Hold.Time, Calls),
-                      weighted.mean(Followup.Time, Calls),
-                      weighted.mean(Total.Call.Time, Calls))
-        ggplot()
+    rpt.var <- reactive({
+        reports[which(reports$name==input$rpts),"var"]
+    })
+    lvl.dtl.func <- reactive({
+        lvl.dtl[which(lvl.dtl$name==input$lvl_dtl),"func"]
+    })
+    
+    assoc.data <- reactive({
+        prod.data %>%
+            filter(Associate==input$assoc) %>%
+            group_by_(interp(~f(Date), f=as.name(lvl.dtl.func()))) %>%
+            summarize_(mean.stats=interp(~weighted.mean(x, w),
+                                         x=as.name(rpt.var()),
+                                         w=as.name("Calls")))
+    })
+    sum.data <- reactive({
+        prod.data %>%
+            group_by_(interp(~f(Date), f=as.name(lvl.dtl.func()))) %>%
+            summarize_(mean.stats=interp(~weighted.mean(x, w),
+                                         x=as.name(rpt.var()),
+                                         w=as.name("Calls")))
+    })
+    
+    # Need selector for stat to examine
+    # Pull field names, add name of stat being calculated
+    # Use selector to add field for stat being calculated
+    # Apply names vector back to dataframe names
+    # Summarize data
+    # Plot data    
+    output$assoc.table <- renderTable({
+        if(input$submit>0)
+            isolate(assoc.data())
+    })
+    output$sum.table <- renderTable({
+        if(input$submit>0)
+            isolate(sum.data())
     })
 })
--- Phone Agent Productivity Coach/ui.R
+++ Phone Agent Productivity Coach/ui.R
@@ -4,14 +4,19 @@ shinyUI(fluidPage(
     titlePanel("Phone Agent Productivity Coach"),
     fluidRow(
         column(2,
-            selectInput("assoc", h4("Associate Name"), choices=NULL)),
+               selectInput("assoc", h4("Associate Name"), choices=NULL)),
         column(4,
-            dateRangeInput("dates", h4("Select Date Range"))),
+               dateRangeInput("dates", h4("Select Date Range")),
+               selectInput("lvl_dtl", h4("Report Granularity"),
+                           choices=NULL)),
+        column(3,
+               selectInput("rpts", h4("Comparison Report"),
+                           choices=NULL)),
         column(2,
                br(),
                br(),
-               #actionButton("submit", "Submit"))
-               submitButton("Submit"))
+               actionButton("submit", "Submit"))
+               #submitButton("Submit"))
     ),
     hr(),
     fluidRow(
@@ -22,5 +27,16 @@ shinyUI(fluidPage(
             htmlOutput("assoc")),
         column(5,
             htmlOutput("dates"))
-    )
+    ),
+    fluidRow(
+        column(3,
+               htmlOutput("lvl_dtl")),
+        column(3,
+               htmlOutput("rpts"))),
+    fluidRow(
+        column(12,
+               tableOutput("assoc.table"))),
+    fluidRow(
+        column(12,
+               tableOutput("sum.table")))
 ))
